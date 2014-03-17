c2afd5c2bf00702e3325c9f11b500f3dda08ff68
Tim Dettmers <tim.dettmers@gmail.com>
2014-03-17 09:14:54 +0100

    Added uniform sqrt weights in mini-MNIST test.

--- tests/miniMNIST_test.cu
+++ tests/miniMNIST_test.cu
@@ -23,8 +23,8 @@ void run_miniMNIST_test(ClusterNet gpus)
 	Matrix *X = read_hdf5((path + "/mnist_mini_X.hdf5").c_str());
 	Matrix *y = read_hdf5((path + "/mnist_mini_y.hdf5").c_str());
 
-	Matrix *w1 = scalarMul(gpus.rand(784,1000),0.4*sqrt(6.0f/(784.0+1000.0)));
-	Matrix *w2 = scalarMul(gpus.rand(1000,10),0.4*sqrt(6.0f/(10.0+1000.0)));
+	Matrix *w1 = gpus.uniformSqrtWeight(784,1000);
+	Matrix *w2 = gpus.uniformSqrtWeight(1000,10);
 	Matrix *m1 = zeros(784,1000);
 	Matrix *m2 = zeros(1000,10);
 	Matrix *ms1 = zeros(784,1000);
@@ -146,9 +146,138 @@ void run_miniMNIST_test(ClusterNet gpus)
 
 
 	ASSERT(train_error < 0.01f,"mini-MNIST train error 10 epochs < 0.01.");
+	ASSERT(cv_error < 0.1825f, "mini-MNIST train error 10 epochs < 0.1825.");
+
+	b.finish_batch_allocator();
+
+	/*
+	Matrix *w1_dist = scalarMul(gpus.rand(784,1000),0.4*sqrt(6.0f/(784.0+1000.0)));
+	Matrix *w2_dist = scalarMul(gpus.rand(1000,10),0.4*sqrt(6.0f/(10.0+1000.0)));
+	Matrix *m1_dist = zeros(784,1000);
+	Matrix *m2_dist = zeros(1000,10);
+	Matrix *ms1_dist = zeros(784,1000);
+	Matrix *ms2_dist = zeros(1000,10);
+	Matrix *grad_w1_ms_dist = zeros(784,1000);
+	Matrix *grad_w2_ms_dist = zeros(1000,10);
+	Matrix *grad_w2_dist = empty(1000,10);
+	Matrix *grad_w1_dist = empty(784,1000);
+	float cv_error = 0.0f;
+	float train_error = 0.0f;
+
+	BatchAllocator b = BatchAllocator();
+	b.init(X, y, 0.2, 32, 64);
+	int epochs  = 10;
+	float learning_rate = 0.003;
+	float momentum = 0.5;
+	for(int EPOCH = 1; EPOCH < epochs; EPOCH++)
+	{
+	  momentum += 0.01;
+	  if(momentum > 0.95) momentum = 0.95;
+	  for(int i = 0; i < b.TOTAL_BATCHES; i++)
+	  {
+
+		  b.allocate_next_batch_async();
+
+		  //nesterov updates
+		  scalarMul(m1,momentum,m1);
+		  scalarMul(m2,momentum,m2);
+		  add(w1,m1,w1);
+		  add(w2,m2,w2);
+
+		  Matrix *d0 = gpus.dropout(b.CURRENT_BATCH,0.2);
+		  //print_gpus_matrix(w1);
+		  Matrix *z1 = gpus.dot(d0, w1);
+		  logistic(z1, z1);
+		  Matrix *d1 = gpus.dropout(z1,0.6);
+		  Matrix *a2 = gpus.dot(d1,w2);
+		  Matrix *out = softmax(a2);
+		  Matrix *t = create_t_matrix(b.CURRENT_BATCH_Y,10);
+
+		  //backprop
+		  Matrix *e1 = sub(out, t);
+		  Matrix *e2 = gpus.dotT(e1, w2);
+		  gpus.Tdot(z1,e1,grad_w2);
+		  logisticGrad(z1,z1);
+		  mul(e2,z1,e2);
+		  gpus.Tdot(b.CURRENT_BATCH,e2,grad_w1);
+
+		  RMSprop_with_nesterov_weight_update(ms1,grad_w1,w1,m1,0.9f,learning_rate,b.CURRENT_BATCH->rows);
+		  RMSprop_with_nesterov_weight_update(ms2,grad_w2,w2,m2,0.9f,learning_rate,b.CURRENT_BATCH->rows);
+
+		  cudaFree(e1->data);
+		  cudaFree(e2->data);
+		  cudaFree(z1->data);
+		  cudaFree(a2->data);
+		  cudaFree(out->data);
+		  cudaFree(t->data);
+		  cudaFree(d0->data);
+		  cudaFree(d1->data);
+
+		  b.replace_current_batch_with_next();
+
+	  }
+
+	  train_error = 0;
+	  for(int i = 0; i < b.TOTAL_BATCHES; i++)
+	  {
+		  b.allocate_next_batch_async();
+
+		  Matrix *a1 = gpus.dot(b.CURRENT_BATCH,w1);
+
+		  logistic(a1, a1);
+		  Matrix *a2 = gpus.dot(a1,w2);
+		  Matrix *out = softmax(a2);
+		  Matrix *result = argmax(out);
+		  Matrix *eq = equal(result,b.CURRENT_BATCH_Y);
+		  float sum_value = sum(eq);
+
+		  train_error += (b.CURRENT_BATCH->rows - sum_value)/ (1.0f * b.CURRENT_BATCH->rows *b.TOTAL_BATCHES) ;
+
+		  cudaFree(a1->data);
+		  cudaFree(a2->data);
+		  cudaFree(out->data);
+		  cudaFree(result->data);
+		  cudaFree(eq->data);
+
+		  b.replace_current_batch_with_next();
+	  }
+
+	  //std::cout << "Train error: " << train_error << std::endl;
+
+	  cv_error = 0;
+	  for(int i = 0; i < b.TOTAL_BATCHES_CV; i++)
+	  {
+		  b.allocate_next_cv_batch_async();
+
+		  Matrix *a1 = gpus.dot(b.CURRENT_BATCH_CV,w1);
+		  logistic(a1, a1);
+		  Matrix *a2 = gpus.dot(a1,w2);
+		  Matrix *out = softmax(a2);
+		  Matrix *result = argmax(out);
+		  Matrix *eq = equal(result,b.CURRENT_BATCH_CV_Y);
+		  float sum_value = sum(eq);
+
+		  cv_error += (b.CURRENT_BATCH_CV->rows  - sum_value)/ (1.0f * b.CURRENT_BATCH_CV->rows *b.TOTAL_BATCHES_CV) ;
+
+		  cudaFree(a1->data);
+		  cudaFree(a2->data);
+		  cudaFree(out->data);
+		  cudaFree(result->data);
+		  cudaFree(eq->data);
+
+		  b.replace_current_cv_batch_with_next();
+	  }
+
+	  //std::cout << "Cross validation error: " << cv_error << std::endl;
+
+	}
+
+
+	ASSERT(train_error < 0.01f,"mini-MNIST train error 10 epochs < 0.01.");
 	ASSERT(cv_error < 0.17f, "mini-MNIST train error 10 epochs < 0.17.");
 
 	b.finish_batch_allocator();
+	*/
 
 
 }
